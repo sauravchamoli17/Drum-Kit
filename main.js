5057aad5fa80db8c0ccba4065a8edb6c74d1aa2d
Saurav Chamoli <sauravchamoli17@gmail.com>
2020-10-30 19:19:52 +0530

    Logic Completed!

---
+++ main.js
@@ -0,0 +1,20 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip it if it's not a transform
+    this.classList.remove('playing');
+}
+
+//Array of Keys
+const keys = document.querySelectorAll('.key'); 
+//Loop over array of keys and remove class of playing after transition
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//Play Sound whenever the key is pressed
+window.addEventListener('keydown',playSound);
